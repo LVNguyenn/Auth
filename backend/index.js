28e35ef46c1fa6f947c9d2995b089b33fe2d25bf
Vi Nguyen <levinguyen170602@gmail.com>
2024-08-15 16:10:20 +0700

    deployment

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import { connectDb } from "./db/connectDb.js";
+
 import authRoutes from "./routes/auth.r.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -17,6 +20,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, async () => {
   await connectDb();
