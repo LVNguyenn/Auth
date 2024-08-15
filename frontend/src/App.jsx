7be1709bfecca0a30d533b6fd4b6171376a94127
Vi Nguyen <levinguyen170602@gmail.com>
2024-08-15 16:21:56 +0700

    update catch all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -85,6 +85,13 @@ function App() {
 						</RedirectAuthenticatedUser>
 					}
 				/>
+        {/* catch all routes */}
+        <Route
+					path='*'
+					element={
+						<Navigate to='/' replace />
+					}
+				/>
       </Routes>
       <Toaster/>
     </div>
